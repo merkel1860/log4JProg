f9bf9de9c05294fc01469c44137c2400cc0b4385
Muller Merkel <merkelmller@yandex.ru>
2020-10-09 20:10:07 -0400

    Updating log4j2.xml by adding an Database Appender. By now, we can insert error logs into logs table within database jammyDB inside the schema log_management.

---
+++ src/main/java/ConnectionFactory.java
@@ -0,0 +1,47 @@
+
+import org.apache.logging.log4j.Logger;
+
+import java.sql.*;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.Executor;
+
+import org.apache.logging.log4j.LogManager;
+
+
+
+public class ConnectionFactory {
+
+    private static Logger logger ;
+    private static Connection connection;
+
+    private static void settingConnectionParameters() {
+        logger = LogManager.getLogger();
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            logger.error(e.getMessage());
+        }
+        String connectionUrl = "jdbc:mysql://localhost:3306/jammyDB?" +
+                "useUnicode=true&characterEncoding=UTF-8&user=nebel&password=nebel1984";
+        try {
+            connection = DriverManager.getConnection(connectionUrl);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+            logger.error(throwables.getMessage());
+        }
+
+    }
+    public static Connection getConnection(){
+        settingConnectionParameters();
+        if (connection == null){
+            logger.error("Connection error, Failed to connect to MySQL");
+            return null;
+        }else {
+            logger.info("Connection to Database successful");
+        }
+
+        return connection;
+    }
+}
